c9547333b9a7dd200bcd8670e6e5a9a3eff230e1
six-ju <yuk1479@naver.com>
2025-04-18 09:49:46 +0900

    [UPDATE] 디비구조 수정

--- migrations/20250418002057-create-season.js
+++ migrations/20250418002057-create-season.js
@@ -2,20 +2,25 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('season', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      seasonName: {
-        allowNull: false,
-        type: Sequelize.STRING
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('season');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('season', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            seasonName: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('season');
+    },
 };
--- migrations/20250418002102-create-player.js
+++ migrations/20250418002102-create-player.js
@@ -2,20 +2,35 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('player', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      playerName: {
-        allowNull: false,
-        type: Sequelize.STRING
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('player');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('player', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            season_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'season',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            playerName: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('player');
+    },
 };
